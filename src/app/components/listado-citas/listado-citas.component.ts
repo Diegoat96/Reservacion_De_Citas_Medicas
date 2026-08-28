527575eb6101055b6e1080faa9e192140d00ebca
ABCardona <benjamincardona.2008@gmail.com>
2026-08-28 02:53:07 -0600

    feat: renderizar tabla de citas desde arreglo en memoria

--- src/app/components/listado-citas/listado-citas.component.ts
+++ src/app/components/listado-citas/listado-citas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cita } from '../../models/cita';
+import { CitasService } from '../../services/citas.service';
 
 @Component({
   selector: 'app-listado-citas',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './listado-citas.component.html',
   styleUrl: './listado-citas.component.css',
 })
-export class ListadoCitasComponent {}
+export class ListadoCitasComponent implements OnInit {
+  protected citas: Cita[] = [];
+
+  constructor(private readonly citasService: CitasService) {}
+
+  ngOnInit(): void {
+    this.actualizarCitas();
+  }
+
+  protected estadoCss(estado: Cita['estado']): string {
+    return estado.toLowerCase();
+  }
+
+  private actualizarCitas(): void {
+    this.citas = this.citasService.obtenerCitas();
+  }
+}
